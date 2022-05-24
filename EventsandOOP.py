d728ecc817d64e566d6547f5578bf0d16c867706
Rhi <rhiannonmaccreadie@gmail.com>
2022-05-24 12:40:00 +1200

    Update EventsandOOP.py

--- EventsandOOP.py
+++ EventsandOOP.py
@@ -8,6 +8,7 @@ class Product:
 
     _name: str
     _price: float
+    _has_sugar: bool
 
     @property
     def name(self) -> str:
@@ -33,7 +34,18 @@ class Product:
         else:
             raise ValueError()
 
-
+    @property
+    def has_sugar(self) -> bool:
+        return self._has_sugar
+    
+    @has_sugar.setter
+    def has_sugar(self, sugar: bool):
+        if sugar == True:
+            self._has_sugar = True
+        
+        else:
+            self._has_sugar = False
+    
 # List of drink products
 products = [
     Product("Long Black", 2.00),
@@ -51,24 +63,27 @@ main_window.setWindowTitle("Shop")
 
 # Set main widget and layout
 main_widget = QWidget()
+right_widget = QWidget()
 main_window.setCentralWidget(main_widget)
 vbox = QVBoxLayout()
-main_widget.setLayout(vbox)
+hbox = QHBoxLayout()
+right_widget.setLayout(vbox)
+main_widget.setLayout(hbox)
 
 # Set up three product widgets
-product_combo = QComboBox()
+product_combo = QListWidget()
 # Adds the names of the products to the combo box
 product_combo.addItems([x.name for x in products])
 name_label = QLabel("Product name")
 price_label = QLabel("Product price")
 
 # Add widgets to vbox layout
-vbox.addWidget(product_combo)
+hbox.addWidget(product_combo)
 vbox.addWidget(name_label)
 vbox.addWidget(price_label)
+hbox.addWidget(right_widget)
 
-
-def product_combo_currentIndexChanged(index: int):
+def product_combo_currentRowChanged(index: int):
     """
     Updates the product labels based on the new index
     that was selected.
@@ -83,10 +98,10 @@ def product_combo_currentIndexChanged(index: int):
 
 
 # Connect the function to the combo box's signal
-product_combo.currentIndexChanged.connect(product_combo_currentIndexChanged)
+product_combo.currentRowChanged.connect(product_combo_currentRowChanged)
 
 # Update the labels manually to the 0th item, Long Black
-product_combo_currentIndexChanged(0)
+product_combo_currentRowChanged(0)
 
 # Show the window
 main_window.show()
